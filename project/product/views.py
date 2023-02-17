375a12b7cfddac6b9ca75a78c566d2835eed84c9
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-02-17 12:20:42 +0000

    Added review in prod functionality to check if onother exist and update the first in the list with form data

--- project/product/views.py
+++ project/product/views.py
@@ -3,6 +3,7 @@ from product.models import Product, Review
 
 # Create your views here.
 def product(request, model):
+
     products = get_object_or_404(Product, model=model)
 
     if request.method == 'POST':
@@ -10,12 +11,25 @@ def product(request, model):
         content = request.POST.get('message', '')
 
         if content:
-            review = Review.objects.create(
-                product=products,
-                rating=rating,
-                content=content,
-                created_by=request.user
-            )
+
+            # check to see if user already has a review in the database
+            reviews = Review.objects.filter(created_by=request.user, product=products)
+
+            # if review already exists for the user get the first one in the list
+            # and update review with the values from the form
+            if reviews.count() > 0:
+                review = reviews.first()
+                review.rating = rating
+                review.content=content
+                review.save()
+            else:
+                # else create new reveiw object
+                review = Review.objects.create(
+                    product=products,
+                    rating=rating,
+                    content=content,
+                    created_by=request.user
+                )
             return redirect('product', model=model)
 
     context = {
