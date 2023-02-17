086f97d8f1948ce8074caf73810c1cd55d7768c6
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-02-17 10:54:24 +0000

    Reviews now submitting to the database

--- project/product/views.py
+++ project/product/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from product.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from product.models import Product, Review
 
 # Create your views here.
 def product(request, model):
     products = get_object_or_404(Product, model=model)
-    
+
+    if request.method == 'POST':
+        rating = request.POST.get('review', 3)
+        content = request.POST.get('message', '')
+
+        if content:
+            review = Review.objects.create(
+                product=products,
+                rating=rating,
+                content=content,
+                created_by=request.user
+            )
+            return redirect('product', model=model)
+
     context = {
         "items": products
     }
